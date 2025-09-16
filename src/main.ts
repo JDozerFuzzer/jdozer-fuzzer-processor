6a61b24db9943b060d912431ae0d3cac6f012fd5
JDozerFuzzer <jdozerfuzzer@gmail.com>
2025-09-16 15:27:10 -0300

    Config logger levels

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ProcessModule } from './jdozer/fuzzer/processor/ProcessModule';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerConfig } from './LoggerConfig';
 
 async function bootstrap() {
 
   try {
-    
+    await ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true
+    });
   } catch (e) {
     console.error('Error setting up configuration:', e);
     process.exit(1);
   }
 
   const app = await NestFactory.create(ProcessModule, {
-    logger: ['error', 'warn', 'log', 'debug', 'verbose']
+    logger: LoggerConfig.logLevels('JDozerFuzzer-Seeder')
   });
 
 }
